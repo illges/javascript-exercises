89ed2c861e6d43b598510713afde05d19d7e37de
Illges <brown1ei.eb@gmail.com>
2018-11-12 14:11:16 -0500

    Completed first few exercises.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let final = '';
+    if (num < 0){
+      final = 'ERROR';
+    } else if (num > 0) {
+      for (let i=0; i<num; i++) {
+        final = final + string;
+      }
+    }
+    return final
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let final='';
+    for (let i=1; i<=string.length; i++) {
+        final = final + string[string.length-i];
+    }
+    return final
 }
 
 module.exports = reverseString

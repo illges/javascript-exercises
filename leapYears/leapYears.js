f7b382acc4bdea05aec193a344e36351113b5ef2
Illges <brown1ei.eb@gmail.com>
2018-11-13 09:10:57 -0500

    Completed leapYears and sumAll exercises.

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  let leapyear;
+  if (year%4===0){
+    if (year%100===0 && year%400!==0){
+      leapyear=false;
+    } else {
+      leapyear=true;
+    }
+  } else {
+    leapyear=false;
+  }
+  return leapyear
 }
 
 module.exports = leapYears
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-  let Test = 'My test commit'
+const sumAll = function(int_1, int_2) {
+  let sum=0;
+  if (typeof int_1 !== "number" ||
+      typeof int_2 !== "number" ||
+      int_1 < 0 || int_2 < 0 ) {
+    sum='ERROR';
+  } else if (int_1 < int_2) {
+    for (let i=int_1; i<=int_2; i++) {
+      sum += i;
+    }
+  } else if (int_2 < int_1) {
+    for (let i=int_2; i<=int_1; i++) {
+      sum += i;
+    }
+  }
+  return sum
 }
 
 module.exports = sumAll

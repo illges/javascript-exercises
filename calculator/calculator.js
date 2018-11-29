8785777713a91f51fb70ceb1940458fe8002f1e9
Illges <brown1ei.eb@gmail.com>
2018-11-29 09:21:12 -0500

    Completed calculator exercise.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,24 +6,25 @@ function subtract (a, b) {
 	return a - b
 }
 
-function sum (obj) {
-	const sum = obj.reduce((total, number) => {
-		return total + number;
-	}, 0);
+function sum (array) {
+	return array.reduce((current, total) => total + current , 0);
 }
 
-function multiply (obj) {
-	const product = obj.reduce((total, number) => {
-		return total * number;
-	}, 1);
+function multiply (array) {
+	return array.reduce((total, current) => total * current, 1);
 }
 
 function power(a, b) {
 	return a ** b
 }
 
-function factorial() {
-	
+function factorial(a) {
+	let fact = 1, i=1;
+	while (i < a+1) {
+		fact *= i;
+		i++;
+	}
+	return fact
 }
 
 module.exports = {

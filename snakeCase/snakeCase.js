19993836e9cd50912a1435d6ec0d916f30ffe529
Illges <brown1ei.eb@gmail.com>
2018-11-29 13:21:32 -0500

    Completed snakeCase exercise.

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,10 @@
-const snakeCase = function() {
+const snakeCase = function(phrase) {
 
+  if (phrase.match(/[^a-zA-Z]/)) {
+    return phrase.split(/[^a-zA-Z]+/).filter(current => (current !== '')).join('_').toLowerCase()
+  } else {
+    return phrase.split(/(?=[A-Z])/).filter(current => (current !== '')).join('_').toLowerCase()
+  }
 }
 
 module.exports = snakeCase

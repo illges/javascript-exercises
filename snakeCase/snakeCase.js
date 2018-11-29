deebf83ac8bfb002ec3d7521df2a89313fecf429
Illges <brown1ei.eb@gmail.com>
2018-11-29 13:31:08 -0500

    Updated snakeCase.js to make chained methods more readable.

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,9 +1,17 @@
 const snakeCase = function(phrase) {
 
   if (phrase.match(/[^a-zA-Z]/)) {
-    return phrase.split(/[^a-zA-Z]+/).filter(current => (current !== '')).join('_').toLowerCase()
+    return phrase
+            .split(/[^a-zA-Z]+/)
+            .filter(current => (current !== ''))
+            .join('_')
+            .toLowerCase();
   } else {
-    return phrase.split(/(?=[A-Z])/).filter(current => (current !== '')).join('_').toLowerCase()
+    return phrase
+            .split(/(?=[A-Z])/)
+            .filter(current => (current !== ''))
+            .join('_')
+            .toLowerCase();
   }
 }
 

6659bb294ecb63813c832ff5c25420be563d768d
Illges <brown1ei.eb@gmail.com>
2018-11-29 10:44:55 -0500

    Completed palindromes exercise.

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
+const palindromes = function(phrase) {
 
+  const array = phrase.split('').filter(char => /^[a-zA-Z]+$/.test(char));
+
+  for (let i = 0; i < phrase.length/2; i++) {
+    if (array[i].toLowerCase() != array[array.length - i - 1].toLowerCase()){
+      return false
+    }
+  }
+  return true
 }
 
 module.exports = palindromes

dfa2178911058af2afd16096bf6c68df9821f682
Illges <brown1ei.eb@gmail.com>
2018-12-01 18:31:16 -0500

    caesar passes all tests but one.

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,14 +1,39 @@
 const caesar = function(string, factor) {
 
   //Creates an Object containing the Alphabet : { 0:'a', 1:'b', etc }
-  let cipher = Object.assign({}, 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split(''));
-
-  //cipher = Object.entries(cipher);
-  //return cipher
+  const cipherUp = Object.assign({}, 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split(''));
+  const cipherLow = Object.assign({}, 'abcdefghijklmnopqrstuvwxyz'.split(''));
 
   return string
           .split('')
-          .map(current => Object.values(cipher).includes(current) )
+          .map(current => { if (Object.values(cipherUp).includes(current)) {
+            if (Object.values(cipherUp).indexOf(current) + factor >= 0) {
+
+              return Object.values(cipherUp)[(Object.values(cipherUp).indexOf(current) + factor) % 26]
+
+            } else {
+
+              return Object.values(cipherUp)[(Object.values(cipherUp).indexOf(current) + factor) % 26 + 26]
+
+            }
+
+          } else if (Object.values(cipherLow).includes(current)) {
+
+            if (Object.values(cipherLow).indexOf(current) + factor >= 0) {
+
+              return Object.values(cipherLow)[(Object.values(cipherLow).indexOf(current) + factor) % 26]
+
+            } else {
+  
+              return Object.values(cipherLow)[(Object.values(cipherLow).indexOf(current) + factor) % 26 + 26]
+
+            }
+
+          } else {
+
+            return current
+
+          }})
           .join('')
 
 }

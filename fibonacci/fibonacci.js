a84a3ee22cdefbe79ba5a3f2ed6eb3713747585c
Illges <brown1ei.eb@gmail.com>
2018-12-01 19:26:08 -0500

    Finished fibonacci exercise.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let fib = [1, 1];
+  if (n < 0) {
+    return 'OOPS'
+  } else {
+    for (let i = 2; i <= n; i++) {
+      fib[i] = fib[i-1]+fib[i-2];
+    }
+    return fib[n-1]
+  }
 }
 
 module.exports = fibonacci

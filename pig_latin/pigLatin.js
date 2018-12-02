db26b6a2cce742ae7946d66e7099fc55940687b1
Illges <brown1ei.eb@gmail.com>
2018-12-01 23:48:32 -0500

    Completed pigLatin exercise.

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,26 @@
-function translate() {
-	// body...
+function translate(string) {
+
+	return string
+					.split(' ')
+					.map(word => {
+						if (!/[aeiou]/.test(word[0])) {
+							let i=0;
+							while (!/[aeiou]/.test(word[i])) {
+								i++
+							}
+							if (word[i]==='u' && word[i-1]==='q') {
+								return word.slice(i+1) + word.slice(0, i+1) + 'ay'
+							} else {
+								return word.slice(i) + word.slice(0, i) + 'ay'
+							}
+						} else {
+							return word + 'ay'
+						}
+					})
+					.join(' ')
 }
 
 
 module.exports = {
 	translate
 }
-
